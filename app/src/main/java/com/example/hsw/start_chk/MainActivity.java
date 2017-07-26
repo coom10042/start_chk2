6ae8b5338e21b53c753c69bb6d3a256b3c4b3eb4
coom10042 <30437410+coom10042@users.noreply.github.com>
2017-07-26 12:14:06 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/hsw/start_chk/MainActivity.java
+++ app/src/main/java/com/example/hsw/start_chk/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private int data = 1234;
-    private String name ="abc";
+    private String name ="def";
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
